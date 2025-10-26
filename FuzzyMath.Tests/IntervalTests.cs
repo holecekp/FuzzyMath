0764b0d6f7de96f061d361949165ec91b7a7ec53
holecekp <24872113+holecekp@users.noreply.github.com>
2025-10-26 18:56:07 +0100

    Unit tests for Interval.Center and Interval.Size

--- FuzzyMath.Tests/IntervalTests.cs
+++ FuzzyMath.Tests/IntervalTests.cs
@@ -7,7 +7,7 @@ namespace Holecek.FuzzyMath.Tests;
 public sealed class IntervalTests
 {
     [TestMethod]
-    public void ValidInterval()
+    public void Constructor_ForValidInterval_SetsProperties_()
     {
         var interval = new Interval(1, 2);
 
@@ -16,11 +16,33 @@ public sealed class IntervalTests
     }
 
     [TestMethod]
-    public void InvalidInterval()
+    public void Constructor_ForInvalidValidInterval_Throws()
     {
         Assert.Throws<ArgumentException>(() =>
         {
             _ = new Interval(1, 0.9);
         });
     }
+
+    [TestMethod]
+    public void Size_ReturnsCorrectValue()
+    {
+        var interval = new Interval(1, 2);
+        double expectedSize = 1;
+
+        double actualSize = interval.Size;
+        Assert.AreEqual(expectedSize, actualSize);
+    }
+
+    [TestMethod]
+    public void Center_ReturnsCorrectValue()
+    {
+        var interval = new Interval(1, 2);
+        const double expectedCenter = 1.5;
+
+        double actualCenter = interval.Center;
+
+        Assert.AreEqual(expectedCenter, actualCenter);
+    }
+
 }
