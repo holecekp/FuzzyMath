a245d2610253f7131f4058094de3a6862a8c3b2b
holecekp <24872113+holecekp@users.noreply.github.com>
2025-12-06 22:59:00 +0100

    A helper class for conversion between alpha-cuts and breaking points notation was added.

---
+++ FuzzyMath/FuzzyNumbers/BreakPointsHelper.cs
@@ -0,0 +1,35 @@
+﻿using Holecek.FuzzyMath.Intervals;
+
+namespace Holecek.FuzzyMath.FuzzyNumbers;
+
+internal static class BreakPointsHelper
+{
+    internal static Interval[] BreakPointsToAlphaCuts(IList<double> breakPoints)
+    {
+        int alphacutsCount = (int)Math.Ceiling(breakPoints.Count / 2.0);
+        var alphaCuts = new Interval[alphacutsCount];
+
+        for (int i = 0; i < alphacutsCount; i++)
+        {
+            double min = breakPoints[i];
+            double max = breakPoints[breakPoints.Count - i - 1];
+            alphaCuts[i] = new Interval(min, max);
+        }
+
+        return alphaCuts;
+    }
+
+    internal static double[] AlphaCutsToBreakPoints(IList<Interval> alphaCuts)
+    {
+        int breakPointsCount = alphaCuts.Count * 2;
+        var breakPoints = new double[breakPointsCount];
+
+        for (int i = 0; i < alphaCuts.Count; i++)
+        {
+            breakPoints[i] = alphaCuts[i].Min;
+            breakPoints[breakPoints.Length - i - 1] = alphaCuts[i].Max;
+        }
+
+        return breakPoints;
+    }
+}
