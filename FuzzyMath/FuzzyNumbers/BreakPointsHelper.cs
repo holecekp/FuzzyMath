6238ab0459cb49563965191b185d072dadae5595
holecekp <24872113+holecekp@users.noreply.github.com>
2025-12-10 17:44:03 +0100

    Fixed logic in the BreakPointsToAlphaCuts for inputs with two breakpoints.

--- FuzzyMath/FuzzyNumbers/BreakPointsHelper.cs
+++ FuzzyMath/FuzzyNumbers/BreakPointsHelper.cs
@@ -6,6 +6,11 @@ internal static class BreakPointsHelper
 {
     internal static Interval[] BreakPointsToAlphaCuts(IList<double> breakPoints)
     {
+        if (breakPoints.Count == 1 || breakPoints.Count == 2)
+        {
+            return BreakPointsToAlphaCuts(new double[] { breakPoints.First(), breakPoints.First(), breakPoints.Last(), breakPoints.Last() });
+        }
+
         int alphacutsCount = (int)Math.Ceiling(breakPoints.Count / 2.0);
         var alphaCuts = new Interval[alphacutsCount];
 
